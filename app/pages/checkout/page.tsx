175483a2c35757415ec0ecdae5bacce75da19741
Josh Mathew <joshmathew2022@gmail.com>
2024-03-22 22:52:33 -0500

    auto-increment checkout-id

--- app/pages/checkout/page.tsx
+++ app/pages/checkout/page.tsx
@@ -13,6 +13,9 @@ function generateRandomUserId(length: number): number {
   const max = Math.pow(10, length) - 1; // Maximum value based on the length
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+var inc:number = 0
+
 //const CheckoutPage: NextPage = () => {
 export default function Checkout(){
   
@@ -21,7 +24,8 @@ export default function Checkout(){
     var total:number
     total = 0;
     var num = user
-
+    var noItems:boolean
+    noItems=false
     //getting products from cart in database
     
     useEffect(()=>{
@@ -33,11 +37,11 @@ export default function Checkout(){
         }})
         .catch((err) => console.log(err));
     },[products]);
-    /*
+    
     if(products?.length ===0){
-        return null
+        noItems=true
     }
-    */
+    
     //removing product from cart
     const removeItem = async (CartID:number) =>{
         axios.delete(`../api/checkout?CartID=${CartID}`)
@@ -79,12 +83,9 @@ export default function Checkout(){
       })
       
       //add each items to order
-
-      
-      var a = 50;
       products?.map((product)=>{
           axios.post('../api/orderItems', {
-            OrderItemID: a++,
+            OrderItemID: inc,
             OrderID: OID,
             ProductID: product.ProductID,
             Quantity: product.Quantity,
@@ -115,61 +116,68 @@ export default function Checkout(){
     })
     
     return (
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div className="">
-          <a href="../">back</a>
-          <div className='mb-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>Checkout</div>
-        </div>
-        <div className="mt-8">
-                <div className="flow-root">
-                  <ul role="list" className="-my-6 divide-y divide-gray-200">
-                    {products?.map((product) => (
-                      <li key={product.CartID} className="flex py-6">
-                        <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                          <img
-                            src={product.ProductDesc}
-                            //alt={product.imageAlt}
-                            className="h-full w-full object-cover object-center"
-                          />
-                        </div>
-
-                        <div className="ml-4 flex flex-1 flex-col">
-                          <div>
-                            <div className="flex justify-between text-base font-medium text-gray-900">
-            
-                              <p className="ml-4">{product.ProductName}</p>
-                            </div>
-
-                          </div>
-                          <div className="flex flex-1 items-end justify-between text-sm">
-                            <p className="text-gray-500">Qty {product.Quantity}</p>
-
-                            <div className="flex">
-                              <button
-                                type="button"
-                                className="font-medium text-indigo-600 hover:text-indigo-500"
-                                onClick={()=>{removeItem(product.CartID)}}
-                              >
-                                Remove
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      </li>
-                    ))}
-                    
-                  </ul>
-                </div>
-              </div>
-              <p className="mt-8">total Price: {total}</p>
-              <button  
-                type = "submit"
-                className="bg-black hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
-                onClick={() =>{addOrder()}}>
-                        Checkout
-              </button>
-        </div>
       
+        <div className="relative isolate px-6 pt-14 lg:px-8">
+          <div className="">
+            <a href="../">back</a>
+            <div className='mb-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>Checkout</div>
+          </div>
+          <div className="mt-8">
+                  <div className="flow-root">
+                    <ul role="list" className="-my-6 divide-y divide-gray-200">
+                      {products?.map((product) => (
+                        <li key={product.CartID} className="flex py-6">
+                          <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                            <img
+                              src={product.ProductDesc}
+                              //alt={product.imageAlt}
+                              className="h-full w-full object-cover object-center"
+                            />
+                          </div>
+
+                          <div className="ml-4 flex flex-1 flex-col">
+                            <div>
+                              <div className="flex justify-between text-base font-medium text-gray-900">
+              
+                                <p className="ml-4">{product.ProductName}</p>
+                              </div>
+
+                            </div>
+                            <div className="flex flex-1 items-end justify-between text-sm">
+                              <p className="text-gray-500">Qty {product.Quantity}</p>
+
+                              <div className="flex">
+                                <button
+                                  type="button"
+                                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                                  onClick={()=>{removeItem(product.CartID)}}
+                                >
+                                  Remove
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                      
+                    </ul>
+                  </div>
+                </div>
+          
+                {(noItems)?
+                  <div>You have no Items in cart</div>:
+                <div>
+                <p className="mt-8">total Price: {total}</p>
+                <button  
+                  type = "submit"
+                  className="bg-black hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+                  onClick={() =>{addOrder()}}>
+                          Checkout
+                </button>
+              </div>}
+          </div>
+
+                    
     );
     
 }
