6648c1dd5ae8bf4dd7339c94218df33c985c2bbf
Josh Mathew <joshmathew2022@gmail.com>
2024-03-23 18:25:16 -0500

    create category within dashboard

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -1,6 +1,7 @@
 import { Category } from '@/types'
 import { NextRequest } from 'next/server'
 import prisma from '@/client'
+import { category } from '@prisma/client'
 
 export async function GET(req: NextRequest) {
     //const searchParams = req.nextUrl.searchParams;
@@ -11,3 +12,14 @@ export async function GET(req: NextRequest) {
     //WHERE CategoryName = ${CategoryName}`
     return new Response(JSON.stringify(id))
 }
+
+export async function POST(req: Request) {
+    const data: category = await req.json();
+    
+    const result = await prisma.$executeRaw`
+    INSERT INTO category(CategoryID,CategoryName)
+    VAlUES(${data.CategoryID},${data.CategoryName});`;
+  
+    //console.log(result)
+    return new Response(JSON.stringify(result));
+  }
--- app/pages/dashboard/page.tsx
+++ app/pages/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { Category } from "@/types";
+
 const addProductsForm = {
     name:"",
     category:"",
@@ -13,18 +14,25 @@ const addProductsForm = {
     expiration: "",
     nutrition:""
 };
-function generateRandomProductId(length: number): number {
+
+const addCategorysForm = {
+    catName:""
+}
+function generateRandomId(length: number): number {
     const min = Math.pow(10, length - 1); // Minimum value based on the length
     const max = Math.pow(10, length) - 1; // Maximum value based on the length
     return Math.floor(Math.random() * (max - min + 1)) + min;
-    const[id, setID] = useState<number>()
 }
 const Dashboard: NextPage = () => {
     var PID:number;
     var CID:number;
     CID = -1;
-    const[formData, setFormData] = useState(addProductsForm)
-    const{name,category,img,price,quantity,expiration,nutrition} = formData;
+    const[addProductsFormData, setFormData] = useState(addProductsForm)
+    const{name,category,img,price,quantity,expiration,nutrition} = addProductsFormData;
+
+    const[addCategoriesFormData, setCategoriesFormData] = useState(addCategorysForm)
+    const{catName} = addCategoriesFormData;
+
 
     const[cat, setCategory] = useState<Category[]>()
 
@@ -38,15 +46,34 @@ const Dashboard: NextPage = () => {
         .catch((err) => console.log(err));
     },[cat]);
     //console.log(cat)
-
+    /*
     const onChange = (e: React.ChangeEvent<HTMLInputElement>)=>{
         setFormData((prevState)=>({
             ...prevState,
             [e.target.id]: e.target.value
         }));
+        
+        setCategoriesFormData((prevState)=>({
+            ...prevState,
+            [e.target.id]: e.target.value
+        }));
+    };
+    */
+    const onProductChange = (e: React.ChangeEvent<HTMLInputElement>)=>{
+        setFormData((prevState)=>({
+            ...prevState,
+            [e.target.id]: e.target.value
+            //name: e.target.value
+        }));
+    };
+    const onCategoryChange = (e: React.ChangeEvent<HTMLInputElement>)=>{
+        setCategoriesFormData((prevState)=>({
+            ...prevState,
+            [e.target.id]: e.target.value
+       }));
     };
 
-    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) =>{
+    const AddProductSubmit = async (e: React.FormEvent<HTMLFormElement>) =>{
         e.preventDefault();
         
         var catID:number
@@ -59,7 +86,7 @@ const Dashboard: NextPage = () => {
             console.log("Category Doesnt Exist, Please create Category first")
             return
         }
-        PID = generateRandomProductId(8);
+        PID = generateRandomId(8);
         axios.post('../api/products', {
             ProductID: PID,
             CategoryID:CID,
@@ -79,6 +106,22 @@ const Dashboard: NextPage = () => {
             toast.error("something went wrong")
         })
     }
+
+    const AddCategorySubmit = async (e: React.FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+        CID = generateRandomId(8);
+        axios.post('../api/category', {
+            CategoryID: CID,
+            CategoryName:catName
+
+        }) .then(()=>{
+            toast("Category added!")
+            setCategoriesFormData(addCategorysForm);
+            //UID= UID+1
+        }) .catch(function(error){
+            toast.error("something went wrong")
+        })
+    }
     return (
         <div className="relative isolate px-6 pt-14 lg:px-8 min-h-screen">
             <a href="../">back</a>
@@ -90,12 +133,12 @@ const Dashboard: NextPage = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                     <div className="p-6 shadow-lg rounded-lg bg-white border border-red-200">
                         <h2 className="text-xl font-semibold">Adding Products</h2>
-                        <form onSubmit={onSubmit} className="mt-5 text-center text-lg leading-9 tracking-tight text-gray-900">
+                        <form onSubmit={AddProductSubmit} className="mt-5 text-center text-lg leading-9 tracking-tight text-gray-900">
                             <label htmlFor="name">
                                 Product Name <span className="text-red-500">*</span>
                             </label>
                             <br />
-                            <input className="border-4 border-black rounded-lg" type="text" id="name" value={name} onChange={onChange} required />
+                            <input className="border-4 border-black rounded-lg" type="text" id="name" value={name} onChange={onProductChange} required />
                             <br />
                             <br />
 
@@ -103,7 +146,7 @@ const Dashboard: NextPage = () => {
                                 Product Category <span className="text-red-500">*</span>
                             </label>
                             <br />
-                            <input className="border-4 border-black rounded-lg" type="text" id="category" value={category} onChange={onChange} required />
+                            <input className="border-4 border-black rounded-lg" type="text" id="category" value={category} onChange={onProductChange} required />
                             <br />
                             <br />
                             
@@ -111,7 +154,7 @@ const Dashboard: NextPage = () => {
                                 Product Image <span className="text-red-500">*</span>
                             </label>
                             <br />
-                            <input className="border-4 border-black rounded-lg" type="testr" id="img" value={img} onChange={onChange} required />
+                            <input className="border-4 border-black rounded-lg" type="testr" id="img" value={img} onChange={onProductChange} required />
                             <br />
                             <br />
 
@@ -119,7 +162,7 @@ const Dashboard: NextPage = () => {
                                 Product Price <span className="text-red-500">*</span>
                             </label>
                             <br />
-                            <input className="border-4 border-black rounded-lg" type="number" id="price" value={price} onChange={onChange} required />
+                            <input className="border-4 border-black rounded-lg" type="number" id="price" value={price} onChange={onProductChange} required />
                             <br />
                             <br />
                             
@@ -127,7 +170,7 @@ const Dashboard: NextPage = () => {
                                 Product Quantity <span className="text-red-500">*</span>
                             </label>
                             <br />
-                            <input className="border-4 border-black rounded-lg" type="number" id="quantity" value={quantity} onChange={onChange} required />
+                            <input className="border-4 border-black rounded-lg" type="number" id="quantity" value={quantity} onChange={onProductChange} required />
                             <br />
                             <br />
                 
@@ -135,7 +178,7 @@ const Dashboard: NextPage = () => {
                                 Expiration Date <span className="text-red-500">*</span>
                             </label>
                             <br />
-                            <input className="border-4 border-black rounded-lg" type="date" id="expiration" value={expiration} onChange={onChange} required />
+                            <input className="border-4 border-black rounded-lg" type="date" id="expiration" value={expiration} onChange={onProductChange} required />
                             <br />
                             <br />
                 
@@ -143,7 +186,7 @@ const Dashboard: NextPage = () => {
                                 Nutrition <span className="text-red-500">*</span>
                             </label>
                             <br />
-                            <input className="border-4 border-black rounded-lg" type="text" id="nutrition" value={nutrition} onChange={onChange} required />
+                            <input className="border-4 border-black rounded-lg" type="text" id="nutrition" value={nutrition} onChange={onProductChange} required />
                             <br />
                             <br />
                 
@@ -156,6 +199,20 @@ const Dashboard: NextPage = () => {
                     <div className="p-6 shadow-lg rounded-lg bg-white border border-red-200">
                         <h2 className="text-xl font-semibold">Creating Category</h2>
                         <p className="mt-2 text-gray-600">Please Note you need to create a category before you add different products</p>
+                        <form onSubmit={AddCategorySubmit} className="mt-5 text-center text-lg leading-9 tracking-tight text-gray-900">
+                            <label htmlFor="catName">
+                                Category Name <span className="text-red-500">*</span>
+                            </label>
+                            <br />
+                            <input className="border-4 border-black rounded-lg" type="text" id="catName" value={catName} onChange={onCategoryChange} required />
+                            <br />
+                            <br />
+                           
+                            <button type="submit" className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+                                Add Category
+                            </button>
+                            <br />
+                        </form>
                     </div>
                     <div className="p-6 shadow-lg rounded-lg bg-white border border-red-200">
                         <h2 className="text-xl font-semibold">Modify Stock Quantity</h2>
